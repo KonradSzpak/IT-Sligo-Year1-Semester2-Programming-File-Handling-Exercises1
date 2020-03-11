b82c8d3f461bc1a6221d00e0662d6ae0f5118079
Konrad Szpak <S00197298@mail.itsligo.ie>
2020-03-11 09:48:33 +0000

    Add project files.

---
+++ fileHandlingEx1/fileHandling.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+using System;
+using System.IO;
+
+
+
+
+namespace fileHandling
+{
+    class fileHandling
+    {
+        static void Main(string[] args)
+        {
+            string lineIn;
+
+
+
+
+            FileStream fs = new FileStream(@"H:\Modules\Year 1\Semester 2\Programming 2 sem2 yr1\idk\readThis.txt", FileMode.Open, FileAccess.Read);
+
+            StreamReader inputStream = new StreamReader(fs);
+
+            lineIn = inputStream.ReadLine();
+
+            Console.WriteLine(lineIn);
+
+            inputStream.Close();
+
+        }
+    }
+}
